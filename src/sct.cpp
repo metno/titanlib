d42aae33ee445b4e5524931775e330f8eb7bca9d
Thomas Nipen <tnipen@gmail.com>
2020-02-21 12:23:15 +0100

    SCT now handles lat/lon as inputs
    Previously, it needed x and y, but now it uses a default projection to
    create x and y. This is a temporary solution.

--- src/sct.cpp
+++ src/sct.cpp
@@ -23,8 +23,12 @@ bool titanlib::sct(const fvec lats,
         fvec& sct,
         ivec& flags) {
 
-    dvec dlats(lats.begin(), lats.end());
-    dvec dlons(lons.begin(), lons.end());
+    fvec x, y;
+    titanlib::util::convert_to_proj(lats, lons, "+proj=lcc +lat_0=63 +lon_0=15 +lat_1=63 +lat_2=63 +no_defs +R=6.371e+06", x, y);
+
+    dvec dx(x.begin(), x.end());
+    dvec dy(y.begin(), y.end());
+
     dvec delevs(elevs.begin(), elevs.end());
     dvec dvalues(values.begin(), values.end());
     dvec dt2pos(t2pos.begin(), t2pos.end());
@@ -50,7 +54,7 @@ bool titanlib::sct(const fvec lats,
     flags.resize(N, 0);
     dsct.resize(N, 0);
 
-    sct_smart_boxes(&N, &dlons[0], &dlats[0], &delevs[0], &dvalues[0], &nmax, &nmin, &nminprof, &ddzmin, &ddhmin, &ddz, &dt2pos[0], &dt2neg[0], &deps2[0], &flags[0], &dsct[0], &rep[0], &boxids[0]);
+    sct_smart_boxes(&N, &dx[0], &dy[0], &delevs[0], &dvalues[0], &nmax, &nmin, &nminprof, &ddzmin, &ddhmin, &ddz, &dt2pos[0], &dt2neg[0], &deps2[0], &flags[0], &dsct[0], &rep[0], &boxids[0]);
     sct = fvec(dsct.begin(), dsct.end());
 
     return true;
