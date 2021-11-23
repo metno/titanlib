13f2cb316243194f63d33646a62bfcae4cd7f87c
Thomas Nipen <tnipen@gmail.com>
2021-11-23 12:16:04 +0100

    Deal with missing elevations in duplicate_check

--- src/duplicate_check.cpp
+++ src/duplicate_check.cpp
@@ -13,23 +13,30 @@ ivec titanlib::duplicate_check(const Points& points, float radius, float vertica
     ivec keep;
     ivec checked(points.size(), 0); // Which points have been checked
     keep.reserve(points.size());
+    bool do_elevation_check = titanlib::is_valid(vertical_range);
+    if(do_elevation_check) {
+        for(int i = 0; i < points.size(); i++) {
+            if(!titanlib::is_valid(elevs[i]))
+                checked[i] = 1;
+        }
+    }
 
     // TODO: Can this be parallelized? Looks difficult...
     for(int i = 0; i < points.size(); i++) {
         if(checked[i] == 1)
             continue;
         ivec indices = points.get_neighbours(lats[i], lons[i], radius, true);
-        int num = indices.size();
+        const int num = indices.size();
         checked[i] = 1;
         keep.push_back(i);
         if(num > 1) {
             // Count how many are within the elevation range
-            // TODO: What should we do if we have a vertical_range, but the elevations are missing?
-            if(titanlib::is_valid(vertical_range) && titanlib::is_valid(elevs[i])) {
-                num = 0;
+            if(do_elevation_check) {
                 for(int j = 0; j < indices.size(); j++) {
                     float curr_elev = elevs[indices[j]];
-                    if(titanlib::is_valid(curr_elev) && (fabs(elevs[i] - curr_elev) <= vertical_range))
+                    if(!titanlib::is_valid(curr_elev))
+                        checked[indices[j]] = 1;
+                    else if(titanlib::is_valid(curr_elev) && (fabs(elevs[i] - curr_elev) <= vertical_range))
                         checked[indices[j]] = 1;
                 }
             }
